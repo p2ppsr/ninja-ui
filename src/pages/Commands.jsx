4448cfd0681670493fddd935d63a818f8b231575
ericx2x <ericjliam@gmail.com>
2021-08-27 23:23:54 -0500

    removed commas

--- src/pages/Commands.jsx
+++ src/pages/Commands.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import utxoninja from 'utxoninja'
 import {
   Typography,
@@ -7,26 +7,49 @@ import {
   Select,
   MenuItem
 } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  params: {
+    margin: '1em auto'
+  },
+  run: {
+    marginBottom: '1em'
+  },
+  results: {
+    boxShadow: theme.shadows[3],
+    margin: '1em auto',
+    minHeight: '5em',
+    padding: '1em',
+    boxSizing: 'border-box',
+    overflow: 'scroll',
+    borderRadius: '0.5em'
+  }
+}), { name: 'Commands' })
+
+const possibleCommands = Object.keys(utxoninja)
 
 const Commands = () => {
-  const possibleCommands = Object.keys(utxoninja)
-  const [command, setCommand] = useState(0)
+  const classes = useStyles()
+  const [command, setCommand] = useState(possibleCommands[0])
   const [params, setParams] = useState('')
-  const [results, setResults] = useState('')
+  const [results, setResults] = useState(
+    'Press "Run Command" to see results...'
+  )
   const [running, setRunning] = useState(false)
 
   const handleRunClick = async () => {
     try {
       setRunning(true)
       const parsedParams = params ? JSON.parse(params) : {}
-      const runResult = await utxoninja[possibleCommands[command]]({
+      const runResult = await utxoninja[command]({
         xprivKey: window.localStorage.xprivKey,
         ...parsedParams
       })
       setResults(JSON.stringify(runResult, null, 2))
     } catch (e) {
-      window.alert('Error: ' + e.message)
       console.error(e)
+      setResults('Error: ' + e.message)
     } finally {
       setRunning(false)
     }
@@ -34,26 +57,37 @@ const Commands = () => {
 
   return (
     <div>
-      <Typography variant='h3'>Commands</Typography>
+      <Typography variant='h3' paragraph>Commands</Typography>
       <Select
-        onChange={(e, v) => setCommand(v)}
+        onChange={(e) => setCommand(e.target.value)}
         value={command}
+        fullWidth
       >
         {possibleCommands.map((x, i) => (
-          <MenuItem key={i} value={i}>{x}</MenuItem>
+          <MenuItem key={i} value={x}>{x}</MenuItem>
         ))}
       </Select>
       <TextField
+        className={classes.params}
         multiline
         label='Params (JSON)'
-        lines={10}
+        rows={10}
         fullWidth
         placeholder='{"newPaymail":"who@johngalt.is"}'
         value={params}
         onChange={e => setParams(e.target.value)}
       />
-      <Button disabled={running} onClick={handleRunClick}>Run</Button>
-      <pre>
+      <Button
+        className={classes.run}
+        disabled={running}
+        onClick={handleRunClick}
+        color='primary'
+        variant='contained'
+      >
+        Run Command
+      </Button>
+      <Typography variant='h4'>Results</Typography>
+      <pre className={classes.results}>
         {results}
       </pre>
     </div>
