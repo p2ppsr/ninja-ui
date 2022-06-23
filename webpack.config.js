277c967b5a08e7466d72ea02883d781603c56725
Ty Everett <ty@tyweb.us>
2022-06-23 12:18:23 -0700

    React Scripts -> Webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/build'), // the bundle output path
+    filename: 'bundle.js' // the name of the bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html' // to import index.html file inside index.js
+    }),
+    new NodePolyfillPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html']
+          }
+        }
+      ]
+    })
+  ],
+  devServer: {
+    port: 3000, // you can change the port
+    client: {
+      overlay: true // Show application errors
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/, // styles files
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: 'url-loader',
+        options: { limit: false }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
